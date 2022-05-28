73be6b2196b1738ba7fae4f3937950874f2d8358
Marcos lobo <marcos.lobo.nit@gmail.com>
2022-05-28 18:17:28 -0300

    Description of site

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import React, { useEffect } from "react";
 
 export default function Index() {
   useEffect(() => {
-    
     const checkIfWalletIsConnected = async () => {
       try {
         if (window.ethereum) {
@@ -36,29 +35,17 @@ export default function Index() {
       <ToastContainer></ToastContainer>
       <h2>Example of voting system on blockchain</h2>
       <p>
-        This example adds a property <code>getLayout</code> to your page,
-        allowing you to return a React component for the layout. This allows you
-        to define the layout on a per-page basis. Since we're returning a
-        function, we can have complex nested layouts if desired.
-      </p>
-      <h3>Vote on some proposals</h3>
-      <p>
-        When navigating between pages, we want to persist page state (input
-        values, scroll position, etc) for a Single-Page Application (SPA)
-        experience.
+        This is an vote smart contract example for study case and complete the
+        etherem101 course.
       </p>
       <p>
-        This layout pattern will allow for state persistence because the React
-        component tree is persisted between page transitions. To preserve state,
-        we need to prevent the React component tree from being discarded between
-        page transitions.
+        It's deployed and running on Rinkebery, one of the Ethereum test network
       </p>
-      <h3>Try It Out</h3>
       <p>
-        To visualize this, try tying in the search input in the{" "}
-        <code>Sidebar</code> and then changing routes. You'll notice the input
-        state is persisted.
+        <b>Sources in</b>
       </p>
+      <p>Smart Contract: https://github.com/marcoslobo/poc-votes-sm</p>
+      <p>Front: https://github.com/marcoslobo/poc-votes-front</p>
     </section>
   );
 }
